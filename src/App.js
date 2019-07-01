aed890bd9f5231cb23cabbd477e4a201b67b6651
kasun <kasunebj@gmail.com>
2019-07-01 16:39:08 +0530

    updated upto state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+class App extends Component {
+
+state = {
+  person:[{name:"kasun",age:"28"},{name:"eranga",age:"25"},{name:"sahan",age:"23"}]
+}
+
+render(){
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi i am a react app</h1>
+      <p>this is really working</p>
+      <button>Switch Name</button>
+      <Person name ={this.state.person[0].name} age={this.state.person[0].age}/>
+      <Person name ={this.state.person[1].name} age={this.state.person[1].age}>My hobbies : wrestling </Person>
+      <Person name ={this.state.person[2].name} age ={this.state.person[2].age}/>
+
     </div>
   );
 }
+
+}
+
 
 export default App;
